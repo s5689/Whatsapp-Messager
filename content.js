a7f8a1b7a19cd2cc73e394cca9d3fe93b8675fd8
s5689 <lemirl0ug@gmail.com>
2025-02-27 17:10:59 -0300

    Copiar RUT Addon
    + Boton para copiar el RUT del paciente al que se le vende en la seccion de Venta.

--- content.js
+++ content.js
@@ -639,8 +639,54 @@ try {
 } catch (e) {}
 
 // Deseleccionar automaticamente el boton de imprimir boleta
+// Copiar RUT
 try {
   if (document.title === 'Venta') {
+    // Boton de Copiar RUT
+    const copyButton = document.createElement('button');
+
+    // Estilo del boton
+    copyButton.setAttribute(
+      'style',
+      'background-color: unset; border: none; width: 25px; height: 29px'
+    );
+
+    // Icono & estilo del boton
+    copyButton.innerHTML =
+      '<svg style="width: 19px; margin-top: 2px;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M384 336l-192 0c-8.8 0-16-7.2-16-16l0-256c0-8.8 7.2-16 16-16l140.1 0L400 115.9 400 320c0 8.8-7.2 16-16 16zM192 384l192 0c35.3 0 64-28.7 64-64l0-204.1c0-12.7-5.1-24.9-14.1-33.9L366.1 14.1c-9-9-21.2-14.1-33.9-14.1L192 0c-35.3 0-64 28.7-64 64l0 256c0 35.3 28.7 64 64 64zM64 128c-35.3 0-64 28.7-64 64L0 448c0 35.3 28.7 64 64 64l192 0c35.3 0 64-28.7 64-64l0-32-48 0 0 32c0 8.8-7.2 16-16 16L64 464c-8.8 0-16-7.2-16-16l0-256c0-8.8 7.2-16 16-16l32 0 0-48-32 0z"/></svg>';
+
+    // Copiar RUT al dar click
+    copyButton.onclick = (e) => {
+      e.preventDefault();
+      const pxText = document.querySelector('#person b').innerHTML;
+      let currentRut = '';
+      let k = 0;
+
+      // Extraer RUT del texto
+      while (true) {
+        const currentChar = pxText.charAt(k);
+
+        if (currentChar !== ' ') {
+          currentRut += currentChar;
+        } else {
+          break;
+        }
+
+        k++;
+      }
+
+      // Copiar y enviar alert.
+      navigator.clipboard.writeText(currentRut).then(() => {
+        alert('RUT copiado al portapapeles.');
+      });
+    };
+
+    // Insertar el boton en el HTML
+    document
+      .querySelector('#cliente_seleccionado')
+      .insertAdjacentElement('beforebegin', copyButton);
+
+    // Deseleccionar boleta
     document.querySelector("table[style='margin-bottom:0px;'] td input").click();
   }
 } catch (e) {}
